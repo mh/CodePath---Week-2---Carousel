5ec811c617f0450ef122bf17ce085b19013d6608
matt hunter <mdothunter@gmail.com>
2015-09-28 13:50:05 -0400

    Initial Commit

---
+++ Carousel-Week2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carousel-Week2
+//
+//  Created by matt hunter on 9/28/15.
+//  Copyright © 2015 matt hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
